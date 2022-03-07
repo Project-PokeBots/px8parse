887f2e8717bc54a6585c8dafd95bf9ce42b3231a
Jan <73044370+Z1R343L@users.noreply.github.com>
2022-03-08 02:08:45 +0300

    0.8.3

--- px8parse/kaitai/pkb8.py
+++ px8parse/kaitai/pkb8.py
@@ -1,27 +1,3 @@
-# MIT License
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-# This file was compiled from a KSY format file downloaded from:
-# https://github.com/Project-PokeBots/kaitai_struct_formats
-
-
 # This is a generated file! Please edit source .ksy file and use kaitai-struct-compiler to rebuild
 
 from pkg_resources import parse_version
@@ -162,7 +138,7 @@ class Pkb8(KaitaiStruct):
             self._read()
 
         def _read(self):
-            self.nickname = (self._io.read_bytes(24)).decode(u"UTF-8")
+            self.nickname = self._io.read_bytes(24)
             self.enc_chk_0 = Pkb8.EncChk(self._io, self, self._root)
             self.moves = [None] * (4)
             for i in range(4):
